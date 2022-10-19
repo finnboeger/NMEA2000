37694f7ff829604f3d3d58076c4dc776c395c9be
Finn Böger <finnboeger@googlemail.com>
2022-10-19 23:18:37 +0200

    set device info in test

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import n2k
 bus = can.Bus('can0', interface='socketcan')
 notifier = can.Notifier(bus, [])
 n2k_node = n2k.Node(bus)
+n2k_node.set_device_information(1)
+n2k_node.set_product_information("Test", "0.0.1", "Dev", "00000000001", 2)
+n2k_node.set_configuration_information()
+
 notifier.add_listener(n2k_node)
 notifier.add_listener(print)
 
