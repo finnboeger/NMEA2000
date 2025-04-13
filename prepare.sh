bd05650cb641b2d4abbdf8515cbf78b60b88c1f2
Finn Böger <finnboeger@googlemail.com>
2025-04-13 21:43:07 +0200

    chore: setup environment via prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -e
+
+# create virtual environment
+python3 -m venv .venv
+
+# install dependencies
+source .venv/bin/activate
+pip3 install -r requirements.dev.txt
 
 # setup git hooks
 pre-commit install
