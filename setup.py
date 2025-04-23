4927016b97ee959a03c8912691e0bf8165ae25de
Finn Böger <finnboeger@googlemail.com>
2025-04-23 23:55:55 +0200

    chore: bump version
    changing the interface of all message creation functions introduced a breaking change.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="n2k",
     packages=find_packages(include=["n2k"]),
-    version="0.1.0",
+    version="0.2.0",
     description="Library to communicate with NMEA2000 devices",
     author="Finn Böger",
     license="MIT",
