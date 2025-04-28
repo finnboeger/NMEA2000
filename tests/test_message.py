638c741949349d86c1d6873a89681292afb9cb97
Finn Böger <finnboeger@googlemail.com>
2025-04-28 23:46:30 +0200

    test: test setting and parsing of special undefined values

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,9 +1,16 @@
+from n2k import constants
 from n2k.message import Message
 from n2k.utils import IntRef
 
 
 def test_float() -> None:
-    for val in [0.0, 1.0, 1.1230000257492065, -123.32099914550781]:
+    for val in [
+        constants.N2K_FLOAT_NA,
+        0.0,
+        1.0,
+        1.1230000257492065,
+        -123.32099914550781,
+    ]:
         msg = Message()
         msg.add_float(val)
         index = IntRef(0)
@@ -11,7 +18,7 @@ def test_float() -> None:
 
 
 def test_1_byte_udouble() -> None:
-    for val in [0.0, 1.502, 2.512]:
+    for val in [constants.N2K_DOUBLE_NA, 0.0, 1.502, 2.512]:
         for precision in [0.1, 0.01]:
             inverse_precision = round(1 / precision)
             ret_val = round(val / precision) / inverse_precision
@@ -23,7 +30,7 @@ def test_1_byte_udouble() -> None:
 
 
 def test_1_byte_double() -> None:
-    for val in [0.0, 1.1256, -1.1256]:
+    for val in [constants.N2K_DOUBLE_NA, 0.0, 1.1256, -1.1256]:
         for precision in [0.1, 0.01]:
             inverse_precision = round(1 / precision)
             ret_val = round(val / precision) / inverse_precision
@@ -35,7 +42,7 @@ def test_1_byte_double() -> None:
 
 
 def test_2_byte_udouble() -> None:
-    for val in [0.0, 1.502, 2.512, 5.123, 20.69584, 652.654]:
+    for val in [constants.N2K_DOUBLE_NA, 0.0, 1.502, 2.512, 5.123, 20.69584, 652.654]:
         for precision in [0.1, 0.01]:
             inverse_precision = round(1 / precision)
             ret_val = round(val / precision) / inverse_precision
@@ -47,7 +54,7 @@ def test_2_byte_udouble() -> None:
 
 
 def test_2_byte_double() -> None:
-    for val in [0.0, 1.1256, -1.1256, 2.512, -237.136]:
+    for val in [constants.N2K_DOUBLE_NA, 0.0, 1.1256, -1.1256, 2.512, -237.136]:
         for precision in [0.1, 0.01]:
             inverse_precision = round(1 / precision)
             ret_val = round(val / precision) / inverse_precision
@@ -59,7 +66,7 @@ def test_2_byte_double() -> None:
 
 
 def test_3_byte_udouble() -> None:
-    for val in [0.0, 1.502, 2.512, 5.123, 20.69584, 16252.654]:
+    for val in [constants.N2K_DOUBLE_NA, 0.0, 1.502, 2.512, 5.123, 20.69584, 16252.654]:
         for precision in [0.1, 0.01, 1e-3]:
             inverse_precision = round(1 / precision)
             ret_val = round(val / precision) / inverse_precision
@@ -71,7 +78,7 @@ def test_3_byte_udouble() -> None:
 
 
 def test_3_byte_double() -> None:
-    for val in [0.0, 1.1256, -1.1256, 2.512, -4237.136]:
+    for val in [constants.N2K_DOUBLE_NA, 0.0, 1.1256, -1.1256, 2.512, -4237.136]:
         for precision in [0.1, 0.01, 1e-3]:
             inverse_precision = round(1 / precision)
             ret_val = round(val / precision) / inverse_precision
@@ -83,7 +90,7 @@ def test_3_byte_double() -> None:
 
 
 def test_4_byte_udouble() -> None:
-    for val in [0.0, 1.502, 2.512, 5.123, 20.69584, 16252.654]:
+    for val in [constants.N2K_DOUBLE_NA, 0.0, 1.502, 2.512, 5.123, 20.69584, 16252.654]:
         for precision in [0.1, 0.01, 1e-3, 1e-4, 1e-5]:
             inverse_precision = round(1 / precision)
             ret_val = round(val / precision) / inverse_precision
@@ -95,7 +102,7 @@ def test_4_byte_udouble() -> None:
 
 
 def test_4_byte_double() -> None:
-    for val in [0.0, 1.1256, -1.1256, 2.512, -4237.136]:
+    for val in [constants.N2K_DOUBLE_NA, 0.0, 1.1256, -1.1256, 2.512, -4237.136]:
         for precision in [0.1, 0.01, 1e-3, 1e-4, 1e-5]:
             inverse_precision = round(1 / precision)
             ret_val = round(val / precision) / inverse_precision
@@ -107,7 +114,15 @@ def test_4_byte_double() -> None:
 
 
 def test_8_byte_double() -> None:
-    for val in [0.0, 1.1256, -1.1256, 2.512, -4237.136, 12456.3245123]:
+    for val in [
+        constants.N2K_DOUBLE_NA,
+        0.0,
+        1.1256,
+        -1.1256,
+        2.512,
+        -4237.136,
+        12456.3245123,
+    ]:
         for precision in [0.1, 0.01, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7]:
             inverse_precision = round(1 / precision)
             ret_val = round(val / precision) / inverse_precision
@@ -119,7 +134,7 @@ def test_8_byte_double() -> None:
 
 
 def test_byte_uint() -> None:
-    for val in [0, 255]:
+    for val in [constants.N2K_UINT8_NA, 0, 255]:
         msg = Message()
         msg.add_byte_uint(val)
         index = IntRef(0)
@@ -127,7 +142,7 @@ def test_byte_uint() -> None:
 
 
 def test_byte_int() -> None:
-    for val in [0, -0x80, 0x7F]:
+    for val in [constants.N2K_INT8_NA, 0, -0x80, 0x7F]:
         msg = Message()
         msg.add_byte_int(val)
         index = IntRef(0)
@@ -135,7 +150,7 @@ def test_byte_int() -> None:
 
 
 def test_2_byte_uint() -> None:
-    for val in [0, 0xFF, 0xFFFF]:
+    for val in [constants.N2K_UINT16_NA, 0, 0xFF, 0xFFFF]:
         msg = Message()
         msg.add_2_byte_uint(val)
         index = IntRef(0)
@@ -143,7 +158,7 @@ def test_2_byte_uint() -> None:
 
 
 def test_2_byte_int() -> None:
-    for val in [0, -0x80, 0x7F, -0x8000, 0x7FFF]:
+    for val in [constants.N2K_INT16_NA, 0, -0x80, 0x7F, -0x8000, 0x7FFF]:
         msg = Message()
         msg.add_2_byte_int(val)
         index = IntRef(0)
@@ -151,7 +166,7 @@ def test_2_byte_int() -> None:
 
 
 def test_3_byte_uint() -> None:
-    for val in [0, 0xFF, 0xFFFF, 0xFFFFFF]:
+    for val in [constants.N2K_UINT24_NA, 0, 0xFF, 0xFFFF, 0xFFFFFF]:
         msg = Message()
         msg.add_3_byte_uint(val)
         index = IntRef(0)
@@ -159,7 +174,16 @@ def test_3_byte_uint() -> None:
 
 
 def test_3_byte_int() -> None:
-    for val in [0, -0x80, 0x7F, -0x8000, 0x7FFF, -0x800000, 0x7FFFFF]:
+    for val in [
+        constants.N2K_INT24_NA,
+        0,
+        -0x80,
+        0x7F,
+        -0x8000,
+        0x7FFF,
+        -0x800000,
+        0x7FFFFF,
+    ]:
         msg = Message()
         msg.add_3_byte_int(val)
         index = IntRef(0)
@@ -167,7 +191,7 @@ def test_3_byte_int() -> None:
 
 
 def test_4_byte_uint() -> None:
-    for val in [0, 0xFF, 0xFFFF, 0xFFFFFF, 0xFFFFFFFF]:
+    for val in [constants.N2K_UINT32_NA, 0, 0xFF, 0xFFFF, 0xFFFFFF, 0xFFFFFFFF]:
         msg = Message()
         msg.add_4_byte_uint(val)
         index = IntRef(0)
@@ -175,7 +199,15 @@ def test_4_byte_uint() -> None:
 
 
 def test_8_byte_uint() -> None:
-    for val in [0, 0xFF, 0xFFFF, 0xFFFFFF, 0xFFFFFFFF, 0xFFFFFFFFFFFFFFFF]:
+    for val in [
+        constants.N2K_UINT64_NA,
+        0,
+        0xFF,
+        0xFFFF,
+        0xFFFFFF,
+        0xFFFFFFFF,
+        0xFFFFFFFFFFFFFFFF,
+    ]:
         msg = Message()
         msg.add_uint_64(val)
         index = IntRef(0)
